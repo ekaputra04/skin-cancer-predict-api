6c22b42e1194ac5cd37eed10ca925bc16ced3a31
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-11-14 10:52:49 +0800

    fix: server change the localhost to public

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
@@ -8,7 +7,7 @@ const InputError = require('../exceptions/InputError');
 (async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -19,9 +18,11 @@ const InputError = require('../exceptions/InputError');
   const model = await loadModel();
   server.app.model = model;
 
-  server.route(routes);  // Akan dibahas lebih lanjut setelah pembahasan extension.
+  server.route(routes);
+
   server.ext('onPreResponse', function (request, h) {
     const response = request.response;
+
     if (response instanceof InputError) {
       const newResponse = h.response({
         status: 'fail',
@@ -30,18 +31,18 @@ const InputError = require('../exceptions/InputError');
       newResponse.code(response.statusCode)
       return newResponse;
     }
+
     if (response.isBoom) {
       const newResponse = h.response({
         status: 'fail',
         message: response.message
       })
-      newResponse.code(response.statusCode)
+      newResponse.code(response.output.statusCode)
       return newResponse;
     }
+
     return h.continue;
   });
-
-
 
   await server.start();
   console.log(`Server start at: ${server.info.uri}`);
